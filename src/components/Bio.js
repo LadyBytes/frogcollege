154f16bdbf875d3b780536f0a99fd5f2a904eabe
l-wagner <lisa@ladybytes.io>
2019-05-05 16:48:14 -0400

    transitions

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -1,27 +1,31 @@
 import React from "react";
 import { withStyles } from "@material-ui/core/styles";
+import Fade from "@material-ui/core/Fade";
 
 const Bio = props => {
-    const { classes } = props;
+    const { classes, name, role, bio } = props;
+    var delay = Math.floor(Math.random() * 900) + 200  
     return (
-        <div className={classes.container}>
-            <div className={classes.headshot}>Headshot</div>
+        <Fade in={true}  timeout={delay}>
+            <div className={classes.container}>
+                <div className={classes.headshot}>Headshot</div>
 
-            <div className={classes.text}>BioBioBioBioBioBio</div>
-            <div className={classes.text}>BioBioBioBioBioBio</div>
-            <div className={classes.text}>BioBioBioBioBioBio</div>
-        </div>
+                <div className={classes.text}>{name}</div>
+                <div className={classes.text}>{role}</div>
+                <div className={classes.text}>{bio}</div>
+            </div>
+        </Fade>
     );
 };
 
 const styles = theme => ({
-    container: {textAlign: "center",},
+    container: { textAlign: "center" },
     headshot: {
-        marginBottom: '1em',
-        fontSize: '1.2em',
+        marginBottom: "1em",
+        fontSize: "1.2em"
     },
     text: {
-        fontSize: '1em',
+        fontSize: "1em"
     },
     paper: {
         padding: theme.spacing.unit * 2,
--- src/pages/About.js
+++ src/pages/About.js
@@ -2,10 +2,9 @@ import React from "react";
 import PropTypes from "prop-types";
 import { withStyles } from "@material-ui/core/styles";
 
-
-
 import Bio from "../components/Bio";
 import { Translate } from "react-localize-redux";
+import Masonry from "react-masonry-css";
 
 import { Link } from "react-router-dom";
 import frog from "../images/frog.png";
@@ -13,38 +12,73 @@ import frog from "../images/frog.png";
 
 import "./About.scss";
 
+var items = [
+  {
+    name: "Katherine Gorham",
+    role: "Creator/Head Writer/Treehugger Girl",
+    bio:
+      "Katherine Gorham is a NYC-based writer, performer, and choreographer with a BA in Theater from an alright school. Frog College is her first and favorite actualized series. She continues her study of comedy and screenwriting in NYC, while working as a choreographer and freelance copywriter. She also teaches ballet to babies in TriBeCa, which, unfortunately, is what she is best at. She’s thrilled to share Frog College with the world."
+  },
+  {
+    name: "Chris Matos",
+    role: "Editor/Frog",
+    bio:
+      "Christopher Matos is a director/writer/producer/actor/editor, born, raised, and residing in New York City. He loves all aspects of production and all avenues of storytelling. He has produced short films with MOVE Collaborative and theatrical plays with You Up Productions. In his free time he plays with cats he is allergic to, edits videos as a freelancer, and is always looking for great new people to collaborate with."
+  },
+  {
+    name: "Meghan Ames",
+    role: "Sound Designer/Amelia",
+    bio:
+      "Meghan is a performer and entertainer currently residing in Etampes, France where she studies movement based performance and clown. When she is not entertaining families at the Long Island Children's Museum Theater, she trains in circus arts including tissu, hammock, and acrobalance: completely useless for a podcast. She continues to study, perform, and develop new work in NY and internationally. Keep an eye out for her artistic website which will include details on these new projects."
+  },
+  {
+    name: "Dan Chaffkin",
+    role: "Sound Designer/Roommate",
+    bio:
+      "Dan Chaffkin is a comedy and math person. He likes doing stand up and improv and making videos. He has been told that he would have a great singing voice if he took some lessons. Dan is a black sash in Shoalin Kung-Fu, and he is developing a fundamental theory of comedy using first-order predicate calculus. He has also been known to make himself laugh for twenty minutes by spelling out 'fart' three times in a game of Bananagrams."
+  },
+  { name: "Sam Kellman", role: "Professor", bio: "" },
+  { name: "Mike Flandrau", role: "Scientist", bio: "" },
+  { name: "Sara Neil", role: "Frog Mom", bio: "" }
+];
+
+var bios = (items = items.map(function(item) {
+  return <Bio name={item.name} role={item.role} bio={item.bio} />;
+}));
+
 function About(props) {
   const { classes } = props;
+
+  const breakpointColumnsObj = {
+    default: 4,
+    1100: 3,
+    700: 2,
+    500: 1
+  };
 
   return (
     <div className="About">
       <div>
         {" "}
         <div className={classes.header}> Frog College </div>
-        <div className={classes.logline}> <Translate id={'about.logline'}/></div>
+        <div className={classes.logline}>
+          {" "}
+          <Translate id={"about.logline"} />
+        </div>
       </div>
       <div>
         <div className={classes.content}>
-          <Translate id={'about.summary'}/>
+          <Translate id={"about.summary"} />
         </div>{" "}
       </div>
-      <div className="bios">
-        <div>
-          <Bio />
-        </div>
-        <div>
-          <div className={classes.divider} />
-          <Bio />
-        </div>
-        <div>
-          <div className={classes.divider} />
-          <Bio />
-        </div>
-        <div>
-          <div className={classes.divider} />
-          <Bio />
-        </div>
-      </div>
+
+      <Masonry
+        breakpointCols={breakpointColumnsObj}
+        className="my-masonry-grid"
+        columnClassName="my-masonry-grid_column"
+      >
+        {bios}
+      </Masonry>
     </div>
   );
 }
@@ -57,13 +91,13 @@ const styles = theme => ({
   header: {
     fontWeight: "bold",
     fontSize: "2em",
-    fontFamily: "'Luckiest Guy', cursive",
+    fontFamily: "'Luckiest Guy', cursive"
   },
   logline: {
     fontSize: "1.3em"
   },
-  content:{
-    width: '40%',
+  content: {
+    width: "40%"
   },
   divider: {
     position: "absolute",
