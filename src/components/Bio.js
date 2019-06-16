f8bf4796faa602224c539f28bd64953d5c9dfd22
Lisa Wagner <wagnerl@mskcc.org>
2019-06-16 17:09:28 -0400

    bio bubbles

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -1,26 +1,56 @@
 import React from "react";
 import { withStyles } from "@material-ui/core/styles";
 import Fade from "@material-ui/core/Fade";
-import Paper from '@material-ui/core/Paper';
+import Paper from "@material-ui/core/Paper";
 
+class Bio extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { hovered: false };
+    }
 
-const Bio = props => {
-    const { classes, name, role, bio, photo } = props;
-    var delay = Math.floor(Math.random() * 900) + 200;
-    return (
-        <Fade in={true} timeout={delay}>
-            <Paper className={classes.container}>
-                <div className={classes.headshot}>
+    onHover = () => {
+        this.setState({
+            hovered: true
+        });
+    };
+    render() {
+        const { classes, name, role, bio, photo } = this.props;
+        var delay = Math.floor(Math.random() * 900) + 200;
+
+        return !this.state.hovered ? (
+            <Fade in={true} timeout={delay}>
+                <div onTouchStart={this.onHover} onMouseEnter={this.onHover} className={classes.headshot}>
                     <img className={classes.headshot} src={photo} />
                 </div>
+            </Fade>
+        ) : (
+            <Fade in={this.state.hovered} timeout={delay}>
+                <Paper className={classes.container}>
+                    <div className={classes.headshot}>
+                        <img className={classes.headshot} src={photo} />
+                    </div>
+                    <div className={classes.text}>{name}</div>
+                    <div className={classes.text}>{role}</div>
+                    <div className={classes.bio}>{bio}</div>
+                </Paper>
+            </Fade>
+        );
+        // return (
+        //     <Fade in={true} timeout={delay}>
+        //         <Paper className={classes.container}>
+        //             <div className={classes.headshot}>
+        //                 <img className={classes.headshot} src={photo} />
+        //             </div>
 
-                <div className={classes.text}>{name}</div>
-                <div className={classes.text}>{role}</div>
-                <div className={classes.bio}>{bio}</div>
-            </Paper>
-        </Fade>
-    );
-};
+        //             <div className={classes.text}>{name}</div>
+        //             <div className={classes.text}>{role}</div>
+        //             <div className={classes.bio}>{bio}</div>
+        //         </Paper>
+        //     </Fade>
+        // );
+    }
+}
 
 const styles = theme => ({
     container: {
