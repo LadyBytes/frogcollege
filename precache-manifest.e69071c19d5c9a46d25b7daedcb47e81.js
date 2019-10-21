48a51cc665c4bb1306939c1ebf9db15652676013
Lisa Wagner <wagnerl@mskcc.org>
2019-10-21 14:08:19 -0400

    Updates

---
+++ precache-manifest.e69071c19d5c9a46d25b7daedcb47e81.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2117f1be29490e6c2ce72d237a8a918",
+    "url": "frogcollege.us/index.html"
+  },
+  {
+    "revision": "83106b35fc4a125712f9",
+    "url": "frogcollege.us/static/css/main.42830359.chunk.css"
+  },
+  {
+    "revision": "b46073e61327adfabfac",
+    "url": "frogcollege.us/static/js/2.1da99ad1.chunk.js"
+  },
+  {
+    "revision": "83106b35fc4a125712f9",
+    "url": "frogcollege.us/static/js/main.0138321a.chunk.js"
+  },
+  {
+    "revision": "dc39497bb2aeb8c5ed98",
+    "url": "frogcollege.us/static/js/runtime-main.f7adb4b8.js"
+  },
+  {
+    "revision": "c2ad179ae43e9888460e9edac1308f73",
+    "url": "frogcollege.us/static/media/chris.c2ad179a.jpg"
+  },
+  {
+    "revision": "0ec0f1734abc629fae39c08b31137756",
+    "url": "frogcollege.us/static/media/dan.0ec0f173.jpg"
+  },
+  {
+    "revision": "102fa10aa0111bb4cd39176987f0fdb7",
+    "url": "frogcollege.us/static/media/drunk.102fa10a.png"
+  },
+  {
+    "revision": "3fb7a0337f85641b270fff27606b254c",
+    "url": "frogcollege.us/static/media/frog.3fb7a033.png"
+  },
+  {
+    "revision": "8f8cc71fcbb228e6b60a5def0f5d3eca",
+    "url": "frogcollege.us/static/media/frogschool.8f8cc71f.png"
+  },
+  {
+    "revision": "108e11686be58369d90251349cae4329",
+    "url": "frogcollege.us/static/media/james.108e1168.jpg"
+  },
+  {
+    "revision": "9db0d91aece8355833391de45a8ede93",
+    "url": "frogcollege.us/static/media/jenn.9db0d91a.jpg"
+  },
+  {
+    "revision": "939608a3400288ede550a1661c16a3af",
+    "url": "frogcollege.us/static/media/kat.939608a3.jpg"
+  },
+  {
+    "revision": "f88cbc8856c960e6563e27e1854f3e51",
+    "url": "frogcollege.us/static/media/kevin.f88cbc88.jpg"
+  },
+  {
+    "revision": "01a156f3ce5971ceff1ea20642ef8eb2",
+    "url": "frogcollege.us/static/media/lisa.01a156f3.jpg"
+  },
+  {
+    "revision": "72db26d6405642b7a22ed27be8e807af",
+    "url": "frogcollege.us/static/media/meghan.72db26d6.jpg"
+  },
+  {
+    "revision": "eff0866380fde12341b659b971d8c6de",
+    "url": "frogcollege.us/static/media/mike.eff08663.jpg"
+  },
+  {
+    "revision": "d8afce6b73cc9f68803a13bee07019e8",
+    "url": "frogcollege.us/static/media/sam.d8afce6b.jpg"
+  },
+  {
+    "revision": "df789f228c2dd69ced2dc4a343b32bd2",
+    "url": "frogcollege.us/static/media/sara.df789f22.jpg"
+  }
+]);
